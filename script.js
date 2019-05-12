2a90fa3525ea7b411c78774d5a43a85083f5f554
anthonymartinez <anthonymk.1@gmail.com>
2019-05-12 20:34:18 +0000

    hastily made commits: pixel grid and colors are roughly implemented

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 document.addEventListener("DOMContentLoaded", function() {
   let grid = []
+  let colors = ["white", "silver", "gray", "black", "red", "maroon", "yellow", "olive", "lime", "green", "aqua", "teal", "blue", "navy", "fuchsia", "purple"]
   let num_row = 50
   let num_col = 50
+  let paint_color = "red"
+  function color(event){
+    console.log(this.nodeName)
+    console.log(event)
+    //this.style.backgroundColor = paint_color;
+
+  }
 
   function render(){
-    for (let i=0; i < num_row; i++){
+    for (let i=0; i < num_row; i++){ // loop through rows, creating column containers
       let target = document.querySelector('#container');
-      let col = document.createElement('div');
-      col.className = "col"
-      target.appendChild(col);
+      let row = document.createElement('div');
+      row.className = "row" //appends .col divs to #container
+      target.appendChild(row);
 
-      grid.push([]);
+      grid.push([]); // adds a column to the data grid
 
-      for (let j=0; j < num_col; j++){
+      for (let j=0; j < num_col; j++){ // loop through columns, adding pixels to column
         let pix = document.createElement('div');
-        pix.style.backgroundColor = "red"
-        pix.className = "pixel"
-        col.appendChild(pix);
+        pix.className = "pixel" // appends .pixel s to .col s 
+        row.appendChild(pix);
         
-        grid[i].push(pix.backgroundColor);
+        grid[i].push(pix.backgroundColor); // records background color in grid. note that 0,0 corresponds to top left pixel (i think?!!?)
+
+        pix.addEventListener("click", color);
       }
+    }
+
+    let color_container = document.querySelector('#colors')
+    for(let i=0; i < colors.length;i++){
+      let choice = document.createElement('div')
+      choice.className = "color-choice rounded-circle"
+      choice.style.backgroundColor = colors[i]
+      choice.style.textColor = colors[i]
+      choice.innerText = colors[i]
+      color_container.appendChild(choice)
+
     }
   }
 
@@ -31,6 +51,7 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
   }
+
   render();
   //to dos:
   //
